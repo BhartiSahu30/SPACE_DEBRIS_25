825e646b0b0f53f3eb9c210bcb4dc862be7eba11
Bharti Sahu <bhartirdsahu@gmail.com>
2026-03-29 12:14:46 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,17 +1,8 @@
 from __future__ import annotations
 
-from .base import ANY
-from .base import default_namespace
-from .base import NamedSignal
-from .base import Namespace
-from .base import Signal
-from .base import signal
+from .__main__ import cli_detect, query_yes_no
 
-__all__ = [
-    "ANY",
-    "default_namespace",
-    "NamedSignal",
-    "Namespace",
-    "Signal",
-    "signal",
-]
+__all__ = (
+    "cli_detect",
+    "query_yes_no",
+)
